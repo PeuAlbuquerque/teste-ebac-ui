27ff22a8217f302bc5afbfd9a4a971918a5f9002
Pedro Albuquerque <pedrohmpadev@gmail.com>
2022-07-28 15:57:11 -0300

    Usando Page Objects

--- cypress/integration/endereco.spec.js
+++ cypress/integration/endereco.spec.js
@@ -1,17 +1,18 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page'
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
-    
+
     beforeEach(() => {
         cy.visit('minha-conta')
-        cy.fixture('perfil').then(dados=>{
+        cy.fixture('perfil').then(dados => {
             cy.login(dados.usuario, dados.senha)
         })
-        
+
     });
-    
-    
-    it('Deve fazer cadastro de faturamento com sucesso', () => {
-        //cadastro de endereço
+
+
+    it.only('Deve fazer cadastro de faturamento com sucesso', () => {
+        EnderecoPage.editarEnderecoFaturamento('Fábio', 'Araujo', 'Google', 'Brasil', 'Rua America', '5552', 'Osasco', 'São Paulo', '01023050', '11965453211', 'FabioA@teste.com')
     });
 });
